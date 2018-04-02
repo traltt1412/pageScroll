b54c6db6aa34eb368bb5a891865626ac77ff79e0
ThanhTra Le <thanhtra.le@ogilvy.com>
2018-04-02 16:31:02 +0700

    update scroll function

--- app/assets/js/modules/pageScroll.js
+++ app/assets/js/modules/pageScroll.js
@@ -4,75 +4,70 @@ export default class pageScroll {
 			return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
 		};
 		const sections = el.querySelectorAll('section');
-		let current;
-		let scrollable = true;
-		let top = el.scrollTop;
-		for (let i = 0; i < sections.length; i++) {
-			if (sections[i].offsetTop == top) {
-				sections[i].classList.add('active');
-			}
-		}
-
-		el.addEventListener('scroll', (e) => {
-			if (scrollable) {
-				// if (e.deltaY > 0) {
-				// 	if (current == sections.length - 1) {
-				// 		return false;
-				// 	}
-				// 	current++;
-				// 	this.scrollToSectionAnimate(el, sections[current].offsetTop);
-				// } else {
-				// 	if (current == 0) {
-				// 		return false;
-				// 	}
-				// 	current--;
-				// 	this.scrollToSectionAnimate(el, sections[current].offsetTop);
-				// }
-				let scrollTo = this.checkNearestSection(sections, el.offsetHeight, el.scrollTop);
-				if (scrollTo) {
-					this.scrollToSectionAnimate(el, scrollTo.offsetTop);
-					console.log(el.querySelector('section.active'));
-					el.querySelector('section.active').classList.remove('active');
-					scrollTo.classList.add('active');
-					scrollable = false;
-					setTimeout(() => {
-						scrollable = true;
-					}, 600);
+		let active = 0,
+			scrollable = true,
+			top = el.scrollTop,
+			scrollDuration = 400;
+		let init = e => {
+			for (let i = 0; i < sections.length; i++) {
+				if (sections[i].offsetTop == top) {
+					sections[i].classList.add('active');
+					active = i;
 				}
 			}
-			// e.preventDefault();
-		});
+			el.addEventListener('wheel', (e) => {
+				e.preventDefault();
+				let deltaY = e.deltaY;
+				if (scrollable) {
+					let scrollTo = calcScrollTop(deltaY);
+					if (scrollTo !== false){
+						scrollable = false;
+						scrollToSectionAnimate(scrollTo, scrollDuration);
+						setTimeout(() => {
+							scrollable = true;
+						}, 600);
+					}else{
+						el.scrollTo(0, el.scrollTop + deltaY);
+					}
+				}
+			});
+		};
+		init();
 
+		let calcScrollTop = deltaY => {
+			if(active > 0 && el.scrollTop + deltaY < sections[active].offsetTop){
+				active--;
+				document.querySelector('section.active').classList.remove('active');
+				sections[active].classList.add('active');
+				return sections[active].offsetTop + sections[active].offsetHeight - el.offsetHeight;
+			}
+			if(active < sections.length - 1 && el.scrollTop + deltaY > sections[active].offsetTop + sections[active].offsetHeight - el.offsetHeight){
+				active++;
+				document.querySelector('section.active').classList.remove('active');
+				sections[active].classList.add('active');
+				return sections[active].offsetTop;
+			}
+			return false;
+		}
 		
-	}
-	scrollToSection(el, top) {
-		el.scrollTo(0, top);
+		let scrollToSectionAnimate = (scrollTo, scrollDuration) => {
+			const scrollHeight = el.scrollTop - scrollTo,
+				scrollStep = Math.PI / (scrollDuration / 15),
+				cosParameter = scrollHeight / 2;
+			var scrollCount = 0,
+				scrollMargin,
+				scrollInterval = setInterval(function () {
+					if ((el.scrollTop != scrollTo) && (Math.abs(el.scrollTop - scrollTo) > 10)) {
+						scrollCount = scrollCount + 1;
+						scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
+						el.scrollTo(0, (scrollTo + scrollHeight - scrollMargin));
+					}
+					else {
+						el.scrollTo(0, scrollTo);
+						clearInterval(scrollInterval);
+					}
+				}, 15);
+		}	
 	}
 
-	scrollToSectionAnimate(el, scrollTo, scrollDuration = 400) {
-		const scrollHeight = el.scrollTop - scrollTo,
-			scrollStep = Math.PI / (scrollDuration / 15),
-			cosParameter = scrollHeight / 2;
-		var scrollCount = 0,
-			scrollMargin,
-			scrollInterval = setInterval(function () {
-				if ((el.scrollTop != scrollTo) && (Math.abs(el.scrollTop - scrollTo) > 10)) {
-					scrollCount = scrollCount + 1;
-					scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
-					el.scrollTo(0, (scrollTo + scrollHeight - scrollMargin));
-				}
-				else {
-					el.scrollTo(0, scrollTo);
-					clearInterval(scrollInterval);
-				}
-			}, 15);
-	}
-	checkNearestSection(sections, vh, scrollTop) {
-		for (let i=0;i<sections.length;i++) {
-			if (Math.abs(sections[i].offsetTop - scrollTop) < vh && !sections[i].hasClass('active')) {
-				return sections[i];
-			}
-		}
-		return false;
-	}
 }
