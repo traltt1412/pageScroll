d3099e54087ba4db6ef34798d15463ce8326168e
ThanhTra Le <thanhtra.le@ogilvy.com>
2018-04-16 10:47:59 +0700

    es6 refactor

--- app/assets/js/modules/pageScroll.js
+++ app/assets/js/modules/pageScroll.js
@@ -3,31 +3,30 @@ export default class pageScroll {
 		Element.prototype.hasClass = function (className) {
 			return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
 		};
-
-		const sections = el.querySelectorAll('section');
-		let active = 0,
-			scrollable = true,
-			top = el.scrollTop,
-			scrollDuration = 400,
-			touchY;
-		let wheelEvent = getWheelEvent();
+		this.sections = el.children;
+		this.active = 0;
+		this.scrollable = true;
+		this.top = el.scrollTop;
+		this.scrollDuration = 400;
+		this.touchY;
+		let wheelEvent = this.getWheelEvent(el);
 		let init = e => {
-			for (let i = 0; i < sections.length; i++) {
-				if (sections[i].offsetTop == top) {
-					sections[i].classList.add('active');
-					active = i;
+			for (let i = 0; i < this.sections.length; i++) {
+				if (this.sections[i].offsetTop == this.top) {
+					this.sections[i].classList.add('active');
+					this.active = i;
 				}
 			}
 			el.addEventListener(wheelEvent, (e) => {
 				e.preventDefault();
 				let deltaY = e.deltaY || -e.wheelDelta;
-				if (scrollable) {
-					let scrollTo = calcScrollTop(deltaY);
+				if (this.scrollable) {
+				let scrollTo = this.calcScrollTop(el, deltaY);
 					if (scrollTo !== false) {
-						scrollable = false;
-						scrollToSectionAnimate(scrollTo, scrollDuration);
+					this.scrollable = false;
+						this.scrollToSectionAnimate(el, scrollTo, this.scrollDuration);
 						setTimeout(() => {
-							scrollable = true;
+						this.scrollable = true;
 						}, 600);
 					} else {
 						setTimeout(() => {
@@ -38,17 +37,17 @@ export default class pageScroll {
 			});
 
 			el.addEventListener('touchstart', e => {
-				touchY = e.touches[0].clientY;
+				this.touchY = e.touches[0].clientY;
 			});
 			el.addEventListener('touchend', e => {
-				let deltaY = -(e.changedTouches[0].clientY - touchY);
-				if (scrollable) {
-					let scrollTo = calcScrollTop(deltaY);
+				let deltaY = -(e.changedTouches[0].clientY -this.touchY);
+				if (this.scrollable) {
+				let scrollTo = this.calcScrollTop(el, deltaY);
 					if (scrollTo !== false) {
-						scrollable = false;
-						scrollToSectionAnimate(scrollTo, scrollDuration);
+					this.scrollable = false;
+						this.scrollToSectionAnimate(el, scrollTo, this.scrollDuration);
 						setTimeout(() => {
-							scrollable = true;
+						this.scrollable = true;
 						}, 600);
 					} else {
 						el.scrollTo(0, el.scrollTop + deltaY);
@@ -57,52 +56,52 @@ export default class pageScroll {
 			});
 		};
 		init();
+	}
+	
+	scrollToSectionAnimate(el, scrollTo, scrollDuration) {
+		const scrollHeight = el.scrollTop - scrollTo,
+			scrollStep = Math.PI / (scrollDuration / 15),
+			cosParameter = scrollHeight / 2;
+		var scrollCount = 0,
+			scrollMargin,
+			scrollInterval = setInterval(function () {
+				if ((el.scrollTop != scrollTo) && (Math.abs(el.scrollTop - scrollTo) > 10)) {
+					scrollCount = scrollCount + 1;
+					scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
+					el.scrollTop = scrollTo + scrollHeight - scrollMargin;
+				}
+				else {
+					el.scrollTop = scrollTo;
+					clearInterval(scrollInterval);
+				}
+			}, 15);
+	}
 
-		function calcScrollTop(deltaY) {
-			if (active > 0 && el.scrollTop + deltaY < sections[active].offsetTop) {
-				el.getElementsByClassName('active')[0].classList.remove('active');
-				sections[--active].classList.add('active');
-				return sections[active].offsetTop + sections[active].offsetHeight - el.offsetHeight;
-			}
-			if (active < sections.length - 1 && el.scrollTop + deltaY > sections[active].offsetTop + sections[active].offsetHeight - el.offsetHeight) {
-				el.getElementsByClassName('active')[0].classList.remove('active');
-				sections[++active].classList.add('active');
-				return sections[active].offsetTop;
-			}
-			return false;
+	calcScrollTop(el, deltaY) {
+		if (this.active > 0 && el.scrollTop + deltaY < this.sections[this.active].offsetTop) {
+			el.getElementsByClassName('active')[0].classList.remove('active');
+			this.sections[--this.active].classList.add('active');
+			return this.sections[this.active].offsetTop + this.sections[this.active].offsetHeight - el.offsetHeight;
 		}
-
-		function scrollToSectionAnimate(scrollTo, scrollDuration) {
-			const scrollHeight = el.scrollTop - scrollTo,
-				scrollStep = Math.PI / (scrollDuration / 15),
-				cosParameter = scrollHeight / 2;
-			var scrollCount = 0,
-				scrollMargin,
-				scrollInterval = setInterval(function () {
-					if ((el.scrollTop != scrollTo) && (Math.abs(el.scrollTop - scrollTo) > 10)) {
-						scrollCount = scrollCount + 1;
-						scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
-						el.scrollTop = scrollTo + scrollHeight - scrollMargin;
-					}
-					else {
-						el.scrollTop = scrollTo;
-						clearInterval(scrollInterval);
-					}
-				}, 15);
+		if (this.active < this.sections.length - 1 && el.scrollTop + deltaY > this.sections[this.active].offsetTop + this.sections[this.active].offsetHeight - el.offsetHeight) {
+			el.getElementsByClassName('active')[0].classList.remove('active');
+			this.sections[++this.active].classList.add('active');
+			return this.sections[this.active].offsetTop;
 		}
+		return false;
+	}
 
-		// detect available wheel event
-		function getWheelEvent() {
-			if ('onwheel' in el) {
-				// spec event type
-				return 'wheel';
-			} else if (el.onmousewheel !== undefined) {
-				// legacy event type
-				return 'mousewheel';
-			} else {
-				// older Firefox
-				return 'DOMMouseScroll';
-			}
+	// detect available wheel event
+	getWheelEvent(el) {
+		if ('onwheel' in el) {
+			// spec event type
+			return 'wheel';
+		} else if (el.onmousewheel !== undefined) {
+			// legacy event type
+			return 'mousewheel';
+		} else {
+			// older Firefox
+			return 'DOMMouseScroll';
 		}
 	}
 }
